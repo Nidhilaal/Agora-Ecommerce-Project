d3b2dbf7d15b26d69d3e98cb1ea94bbbd6cc741e
Nidhilal MS <msnidhilal16@gmail.com>
2023-07-14 12:36:32 +0530

    Added address deletion for user

--- src/main/java/com/ecommerce/beta/controller/ShopController.java
+++ src/main/java/com/ecommerce/beta/controller/ShopController.java
@@ -15,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -269,6 +270,12 @@ public class ShopController {
             }
 
         return "redirect:/profile";
+    }
+    
+    @GetMapping("/address/delete/{id}")
+    public String deleteAddress(@PathVariable("id") UUID uuid ) {
+    	addressService.deleteById(uuid);
+    	return "redirect:/profile";
     }
 		
 }
